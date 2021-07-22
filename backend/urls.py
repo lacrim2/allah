aecb6e2038fe060a3017b53f9d0bb4461b6af8de
lacrim2 <87776482+lacrim2@users.noreply.github.com>
2021-07-22 02:12:55 +0200

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -5,6 +5,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('react/',TemplateView.as_view(template_name='index.html')),
+    path('',TemplateView.as_view(template_name='index.html')),
     path('polls/',include('polls.urls')),
 ]
